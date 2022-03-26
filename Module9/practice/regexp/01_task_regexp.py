b59d0a3d762a5833cc8dffc93f932008b876ef63
Navai <100474517+NavaiRus@users.noreply.github.com>
2022-03-26 15:58:33 +0300

    Update 01_task_regexp.py

--- Module9/practice/regexp/01_task_regexp.py
+++ Module9/practice/regexp/01_task_regexp.py
@@ -1,9 +1,13 @@
-# Дано произвольное предложение. Слова разделены пробелами. Предложение содержит знаки препинания.
-# Найдите:
-# 1) первое слово из строки
-# 2) первые два символа каждого слова
-# 3) все слова начинающиеся на гласную букву
-# 4) все слова начинающиеся на согласную букву
-# 5) все уникальные(без дубликатов) знаки препинания
+import re
 
-text = ...
+text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+p= r"\b[A-Z]\w{4}\b"
+p1= r"\b\w{2}"
+p2= r"\b[aeioyu]\w+\b"
+p3= r"\b[^ aeioyu]\w+\b"
+p4= r"[^ \w ]"
+print(re.findall(p, text))
+print(re.findall(p1, text))
+print(re.findall(p2, text))
+print(re.findall(p3, text))
+print(re.findall(p4, text))
